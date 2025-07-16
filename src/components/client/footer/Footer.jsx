d2f7eb74f7e24763265df1f45a201925b6575e38
bhuwanoj_ <bhuwanojha10@gmail.com>
2025-07-16 22:16:25 +0545

    footer component

--- src/components/client/footer/Footer.jsx
+++ src/components/client/footer/Footer.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>footer</div>;
+  return (
+    <section className="min-h-[250px] w-screen bg-blue-950 text-white py-8 flex items-center">
+      <div className="flex h-full w-full items-center justify-around flex-col md:flex-row">
+        {/* logo  */}
+        <div>Logo</div>
+        {/* important links */}
+        <div> important links</div>
+        {/* contact */}
+        <div> Contact info</div>
+        {/* social media */}
+        <div>social media</div>
+      </div>
+    </section>
+  );
 };
 
 export default Footer;
